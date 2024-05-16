27e47c669ec06b5f0f9e964eb46dccb41f1bcb22
Priyank Rajai <priyank73@hotmail.com>
2024-05-16 23:12:07 +0530

    Fix: Import issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Repl from "./Repl";
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const exampleFilesList = await fs.readdir(`./src/examples`);
+  const exampleFilesList = await fs.readdir(`src/examples`);
   const exampleFiles: Record<string, string> = {};
   for (const file of exampleFilesList) {
-    exampleFiles[file] = await fs.readFile(`./src/examples/${file}`, "utf-8");
+    exampleFiles[file] = await fs.readFile(`src/examples/${file}`, "utf-8");
   }
 
   return (
